c514fb0b0cfc55e74a74e92dd70438403d952c75
Lee Yun Seok <leeyj6457@gmail.com>
2025-12-22 01:34:31 +0900

    DrawDebug 주석처리

--- Source/Dungeon_Armory/Private/Characters/Mob/Component/MobAttackComponent.cpp
+++ Source/Dungeon_Armory/Private/Characters/Mob/Component/MobAttackComponent.cpp
@@ -125,34 +125,34 @@ void UMobAttackComponent::OnAttack()
 
 	FColor TraceColor = bHit ? FColor::Red : FColor::Green;
 
-#if WITH_EDITOR
-	DrawDebugCapsule(
-		GetWorld(),
-		(Start + End) * 0.5f,
-		TraceDistance * 0.5f,
-		Radius,
-		FRotationMatrix::MakeFromZ(End - Start).ToQuat(),
-		TraceColor,
-		false,
-		0.25f
-	);
-#endif
+//#if WITH_EDITOR
+//	DrawDebugCapsule(
+//		GetWorld(),
+//		(Start + End) * 0.5f,
+//		TraceDistance * 0.5f,
+//		Radius,
+//		FRotationMatrix::MakeFromZ(End - Start).ToQuat(),
+//		TraceColor,
+//		false,
+//		0.25f
+//	);
+//#endif
 
 	if (bHit)
 	{
 		for (const FHitResult& Hit : HitResults)
 		{
-#if WITH_EDITOR
-			DrawDebugSphere(
-				GetWorld(),
-				Hit.ImpactPoint,
-				1.0f,
-				12,
-				FColor::Red,
-				false,
-				0.25f
-			);
-#endif
+//#if WITH_EDITOR
+//			DrawDebugSphere(
+//				GetWorld(),
+//				Hit.ImpactPoint,
+//				1.0f,
+//				12,
+//				FColor::Red,
+//				false,
+//				0.25f
+//			);
+//#endif
 
 			AActor* HitActor = Hit.GetActor();
 			if (!HitActor)
