f8d91d277c3d4b57e196c041cda5e686c74b8dfd
Lee Yun Seok <leeyj6457@gmail.com>
2026-01-09 23:08:12 +0900

    어택 컴포넌트 미생성 버그, 변수명 바꿔서 해결

--- Source/Dungeon_Armory/Private/Characters/Mob/AIController/MobAIController.cpp
+++ Source/Dungeon_Armory/Private/Characters/Mob/AIController/MobAIController.cpp
@@ -46,7 +46,7 @@ void AMobAIController::OnPossess(APawn* InPawn)
     if (MobBase)
     {
         StatComponent = MobBase->StatComponent;
-        MobAttackComponent = Cast<UMobAttackComponent>(MobBase->AttackComponent);
+        MobAttackComponent = Cast<UMobAttackComponent>(MobBase->_AttackComponent);
     }
 
     // 비헤이비어 트리 실행
@@ -116,6 +116,10 @@ void AMobAIController::Tick(float DeltaTime)
         if (Distance <= StatComponent->AttackableDistance)  // 공격 범위 내라면
         {
             MobState = EMobState::Battle;
+        }
+        else
+        {
+            MobState = EMobState::Chase;
         }
     }
     else
