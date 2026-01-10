2083395862859f9db331eed26d8893b059a0b8cf
Lee Yun Seok <leeyj6457@gmail.com>
2026-01-11 00:43:10 +0900

    뷰 모드 트리거 및 컴포넌트 로직 리팩터링
    1. BeginPlay 시 초기 오버랩 상태를 검사하도록 ViewModeTriggerBox 로직 개선
    2. 불필요한 캐스팅 제거로 오버랩 처리 흐름 단순화
    3. 사용되지 않는 트랜스폼 관련 함수들을 주석 처리하여 ViewModeComponent 로직 정리
    4. 뷰 모드 관련 로직의 가독성을 높이기 위한 처리 흐름 조정
    5. 열거형(Enum) 디스플레이 이름 정리 및 SetViewMode를 블루프린트에 노출

--- Source/Dungeon_Armory/Private/Characters/Mannequin/ViewMode/ViewModeTriggerBox.cpp
+++ Source/Dungeon_Armory/Private/Characters/Mannequin/ViewMode/ViewModeTriggerBox.cpp
@@ -3,8 +3,7 @@
 #include "Characters/Mannequin/ViewMode/ViewModeTriggerBox.h"
 #include "Characters/Mannequin/ViewMode/ViewModeComponent.h"
 #include "GameFramework/Character.h"
-
-class AManny;
+#include "Characters/Mannequin/Manny.h"
 
 AViewModeTriggerBox::AViewModeTriggerBox()
 {
@@ -16,31 +15,44 @@ void AViewModeTriggerBox::BeginPlay()
     Super::BeginPlay();
 
     OnActorBeginOverlap.AddDynamic(this, &AViewModeTriggerBox::OnTriggerBeginOverlap);
-    OnActorEndOverlap.AddDynamic(this, &AViewModeTriggerBox::OnTriggerEndOverlap);
+	OnActorEndOverlap.AddDynamic(this, &AViewModeTriggerBox::OnTriggerEndOverlap);
+
+    // 다음 프레임에 검사
+    GetWorldTimerManager().SetTimerForNextTick(this, &AViewModeTriggerBox::CheckInitialOverlap);
+}
+
+void AViewModeTriggerBox::CheckInitialOverlap()
+{
+   TArray<AActor*> OverlappingActors;
+   GetOverlappingActors(OverlappingActors, AManny::StaticClass());
+
+   for (AActor* Actor : OverlappingActors)
+   {
+       UViewModeComponent* ViewModeComponent = Actor->FindComponentByClass<UViewModeComponent>();
+       if (ViewModeComponent)
+       {
+           ViewModeComponent->SetViewMode(EViewMode::FPS);
+           return;
+       }
+   }
 }
 
 void AViewModeTriggerBox::OnTriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
 {
-    ACharacter* Character = Cast<ACharacter>(OtherActor);
-    if (!Character)
-        return;
-
-    UViewModeComponent* ViewModeComponent = Character->FindComponentByClass<UViewModeComponent>();
+    UViewModeComponent* ViewModeComponent = OtherActor->FindComponentByClass<UViewModeComponent>();
     if (ViewModeComponent)
     {
+		UE_LOG(LogTemp, Warning, TEXT("Enter Trigger Box"));
         ViewModeComponent->SetViewMode(EViewMode::FPS); // 실내로 진입
     }
 }
 
 void AViewModeTriggerBox::OnTriggerEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
 {
-    ACharacter* Character = Cast<ACharacter>(OtherActor);
-    if (!Character)
-        return;
-
-    UViewModeComponent* ViewModeComponent = Character->FindComponentByClass<UViewModeComponent>();
+    UViewModeComponent* ViewModeComponent = OtherActor->FindComponentByClass<UViewModeComponent>();
     if (ViewModeComponent)
     {
+		UE_LOG(LogTemp, Warning, TEXT("Exit Trigger Box"));
         ViewModeComponent->SetViewMode(EViewMode::TPS); // 실외로 나감
     }
 }
